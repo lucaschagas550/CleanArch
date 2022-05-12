68ded5db4bf3f58757377f8e4a7e8448d4adfe6e
DESKTOP-6U832JM\Lucas Chagas <lucasandrade595@gmail.com>
2022-05-12 12:10:51 -0300

    Feat: Configurando as entidades usando Fluent Api.

---
+++ CleanArchMvc.Infra.Data/EntitiesConfiguration/CategoryConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using CleanArchMvc.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.Infra.Data.EntitiesConfiguration
+{
+    //Fluent Api para configurar as tabelas geradas pelo identity
+    public class CategoryConfiguration : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Name).HasMaxLength(100).IsRequired();
+
+            //Popula a tabela ao executar a migration se elas não existirem na tabela
+            builder.HasData(
+                    new Category(1, "Material Escolar"),
+                    new Category(2, "Eletrônicos"),
+                    new Category(3, "Acessórios")
+                );
+        }
+    }
+}
