9beba6182ce026791c2b2cde392ec54acc48fdb5
DESKTOP-6U832JM\Lucas Chagas <lucasandrade595@gmail.com>
2022-05-18 11:27:59 -0300

    Feat: Atualizar um produto.

--- CleanArchMvc.WebUI/Controllers/ProductsController.cs
+++ CleanArchMvc.WebUI/Controllers/ProductsController.cs
@@ -47,5 +47,30 @@ namespace CleanArchMvc.WebUI.Controllers
             }
             return View(productDto);
         }
+
+        [HttpGet()]
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null) return NotFound();
+            var productDto = await _productService.GetById(id);
+
+            if (productDto == null) return NotFound();
+
+            var categories = await _categoryService.GetCategories();
+            ViewBag.CategoryId = new SelectList(categories, "Id", "Name", productDto.CategoryId);
+
+            return View(productDto);
+        }
+
+        [HttpPost()]
+        public async Task<IActionResult> Edit(ProductDTO productDto)
+        {
+            if (ModelState.IsValid)
+            {
+                await _productService.Update(productDto);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(productDto);
+        }
     }
 }
