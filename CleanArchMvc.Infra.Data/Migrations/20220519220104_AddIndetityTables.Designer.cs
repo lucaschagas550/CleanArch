ec1dad1b5af74ef73a526f26288fa926ba528a7b
DESKTOP-M79GPG6\lucas <Lucasandrade595@gmail.com>
2022-05-19 19:06:52 -0300

    Feat: Adicionando identity na aplicação.

--- CleanArchMvc.Infra.Data/Migrations/20220519220104_AddIndetityTables.Designer.cs
+++ CleanArchMvc.Infra.Data/Migrations/20220519220104_AddIndetityTables.Designer.cs
@@ -4,14 +4,16 @@ using CleanArchMvc.Infra.Data.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace CleanArchMvc.Infra.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220519220104_AddIndetityTables")]
+    partial class AddIndetityTables
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
