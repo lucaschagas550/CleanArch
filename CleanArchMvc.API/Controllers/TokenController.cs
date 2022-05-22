78e333944ba79de224fd3c29918e7ca166bb894e
DESKTOP-M79GPG6\lucas <Lucasandrade595@gmail.com>
2022-05-22 15:39:20 -0300

    Feat: Criando controller para o token.

---
+++ CleanArchMvc.API/Controllers/TokenController.cs
@@ -0,0 +1,32 @@
+﻿using CleanArchMvc.API.Models;
+using CleanArchMvc.Domain.Account;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.API.Controllers
+{
+    public class TokenController : Controller
+    {
+        private readonly IAuthenticate _authentication;
+        public TokenController(IAuthenticate authentication)
+        {
+            _authentication = authentication;
+        }
+        [HttpPost("LoginUser")]
+        public async Task<ActionResult<UserToken>> Login([FromBody] LoginModel userInfo)
+        {
+            var result = await _authentication.Authenticate(userInfo.Email, userInfo.Password);
+
+            if (result)
+            {
+                //return GenerateToken(userInfo);
+                return Ok($"User {userInfo.Email} login successfully");
+            }
+            else
+            {
+                ModelState.AddModelError(string.Empty, "Invalid Login attempt.");
+                return BadRequest(ModelState);
+            }
+        }
+    }
+}
